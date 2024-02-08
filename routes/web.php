ed8f06be533f5eb15dd2211882c95e32ddbb21b9
Lucas Barbosa <lucas.barbosa@atlastechnol.com>
2024-02-07 23:52:09 -0300

    refact: install horizon and use batch

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Jobs\MakeOrderJob;
+use App\Jobs\RunPaymentJob;
+use App\Jobs\ValidateCardJob;
 use App\Jobs\SendNotificationJob;
+use Illuminate\Support\Facades\Bus;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -21,6 +25,16 @@ Route::get('/', function () {
 
 Route::get('send-notification-all', function () {
     SendNotificationJob::dispatch();
+});
+
+Route::get('run-batch', function () {
+
+    Bus::batch([
+        new MakeOrderJob,
+        new RunPaymentJob,
+        new ValidateCardJob
+    ])->name('Run Batch', rand(1,10))->dispatch();
+
 });
 
 Auth::routes();
